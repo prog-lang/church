cf0640f42befe157d29e01e449f382392a451792
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2023-02-23 20:42:10 +0300

    feat(wasm): empty module code generator

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ extern crate pest_derive;
 
 mod ast;
 mod parser;
+mod wasm;
 
-use std::fs;
+use std::{
+    fs,
+    io::{stdout, Write},
+};
 
 use parser::ChurchParser;
+use wasm::Module;
 
 fn main() {
     let path = std::env::args().nth(1).expect("source path missing");
     let src = fs::read_to_string(path).expect("failed to read source file");
     let ast = ChurchParser::parse_string(src).expect("failed to parse source file");
-    println!("{:#?}", ast);
+    let wasm = Module::from(&ast).to_wasm();
+    stdout().write_all(wasm.as_slice());
 }
---
+++ wat/const.wat
@@ -0,0 +1,7 @@
+(module
+  (type (func (result i32)))
+  (func (type 0) (result i32)
+    i32.const 42
+    return
+  )
+)
