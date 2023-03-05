65a5ad64bc33c3c9a85b3a209709674c306cbca1
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2023-03-05 17:26:54 +0400

    refactor(ast): use Expr in AST instead of Value

--- src/ast.rs
+++ src/ast.rs
@@ -12,25 +12,15 @@ use crate::types::{self, Type};
 
 #[derive(Default, Debug, PartialEq)]
 pub struct AST {
-    pub module: ModuleHeader,
-    pub declarations: HashMap<String, Declaration>,
+    module: Header,
+    decls: Vec<Decl>,
 }
 
 #[derive(Default, Debug, PartialEq)]
-pub struct ModuleHeader {
-    pub name: String,
-    pub exports: HashSet<String>,
+struct Header {
+    name: String,
+    exports: HashSet<String>,
 }
-
-#[derive(Default, Debug, PartialEq)]
-pub struct Declaration {
-    pub index: usize,
-    pub name: String,
-    pub value: Value,
-}
-
-#[derive(Default, Debug, PartialEq)]
-pub struct Value(i32);
 
 impl TryFrom<Pairs<'_, Rule>> for AST {
     type Error = String;
@@ -38,87 +28,27 @@ impl TryFrom<Pairs<'_, Rule>> for AST {
     fn try_from(pairs: Pairs<Rule>) -> Result<Self, Self::Error> {
         let mut ast = Self::default();
 
-        for (index, pair) in pairs.into_iter().filter(parser::is_not_eoi).enumerate() {
-            ast.declaration(index, pair);
+        for pair in pairs.into_iter().filter(parser::is_not_eoi) {
+            ast.declaration(pair);
         }
 
-        ast.validate()
+        Ok(ast)
     }
 }
 
 impl AST {
-    fn declaration(&mut self, index: usize, pair: Pair<Rule>) {
+    fn declaration(&mut self, pair: Pair<Rule>) {
         match pair.as_rule() {
-            Rule::module => self.module = ModuleHeader::from(pair.into_inner()),
-            Rule::declaration => {
-                let mut decl = Declaration::from(pair.into_inner());
-                decl.index = index - 1; // module header is the first declaration
-                self.declarations.insert(decl.name.clone(), decl);
-            }
+            Rule::module => self.module = Header::from(pair.into_inner()),
+            Rule::declaration => self.decls.push(pair.into_inner().into()),
             _ => unreachable!(),
         }
     }
-
-    fn validate(self) -> Result<Self, String> {
-        for export in self.module.exports.iter() {
-            if !self.declarations.contains_key(export) {
-                return Err(format!(
-                    "module '{}' exports an unknown identifier '{}'.",
-                    self.module.name, export
-                ));
-            }
-        }
-        Ok(self)
-    }
-
-    fn sorted_declarations(&self) -> Vec<&Declaration> {
-        let mut declarations: Vec<&Declaration> = self.declarations.values().collect();
-        declarations.sort_by_key(|decl| decl.index);
-        declarations
-    }
 }
 
-impl Into<Vec<u8>> for AST {
-    fn into(self) -> Vec<u8> {
-        let mut module = Module::new();
-
-        let mut types = TypeSection::new();
-        types.function(vec![], vec![ValType::I32]);
-
-        let mut functions = FunctionSection::new();
-        let type_index = 0;
-        for _ in self.declarations.iter() {
-            functions.function(type_index);
-        }
-
-        let mut exports = ExportSection::new();
-        for decl in self.declarations.values() {
-            if self.module.exports.contains(&decl.name) {
-                exports.export(&decl.name, ExportKind::Func, decl.index as u32);
-            }
-        }
-
-        let mut codes = CodeSection::new();
-        for decl in self.sorted_declarations() {
-            let locals = vec![];
-            let mut f = Function::new(locals);
-            f.instruction(&Instruction::I32Const(decl.value.0));
-            f.instruction(&Instruction::Return);
-            f.instruction(&Instruction::End);
-            codes.function(&f);
-        }
-
-        module.section(&types);
-        module.section(&functions);
-        module.section(&exports);
-        module.section(&codes);
-        module.finish()
-    }
-}
-
-impl From<Pairs<'_, Rule>> for ModuleHeader {
+impl From<Pairs<'_, Rule>> for Header {
     fn from(pairs: Pairs<Rule>) -> Self {
-        let mut module = ModuleHeader::default();
+        let mut module = Header::default();
 
         for pair in pairs {
             match pair.as_rule() {
@@ -132,26 +62,40 @@ impl From<Pairs<'_, Rule>> for ModuleHeader {
     }
 }
 
-impl From<Pairs<'_, Rule>> for Declaration {
+impl From<Pairs<'_, Rule>> for Decl {
     fn from(pairs: Pairs<Rule>) -> Self {
-        let mut declaration = Declaration::default();
+        let mut name = String::new();
+        let mut expr = Expr::I32(0);
 
         for pair in pairs {
             match pair.as_rule() {
-                Rule::name => declaration.name = parser::id(pair),
-                Rule::integer => declaration.value = Value(parser::int(pair)),
+                Rule::name => name = parser::id(pair),
+                Rule::integer => expr = Expr::I32(parser::int(pair)),
                 _ => unreachable!(),
             }
         }
 
-        declaration
+        Decl { name, expr }
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Decl {
+    pub name: String,
+    pub expr: Expr,
+}
+
+impl types::Match for Decl {
+    fn check_type(&self, env: &types::Env, t: Type) -> Result<(), Type> {
+        self.expr.check_type(env, t)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 enum Expr {
-    I32(i32),
-    Name(String),
-    Func(String, Box<Expr>), //* name -> Expr
+    I32(i32),                //* -42
+    Name(String),            //* x
+    Func(String, Box<Expr>), //* x -> Expr
 }
 
 impl types::Match for Expr {
@@ -242,5 +186,47 @@ mod tests {
                 .unwrap_err(),
             Type::Func(Type::I32.into(), Type::Unknown.into()),
         );
+    }
+}
+
+/* AST -> WASM */
+impl Into<Vec<u8>> for AST {
+    fn into(self) -> Vec<u8> {
+        let mut module = Module::new();
+
+        let mut types = TypeSection::new();
+        types.function(vec![], vec![ValType::I32]);
+
+        let mut functions = FunctionSection::new();
+        let type_index = 0;
+        for _ in self.decls.iter() {
+            functions.function(type_index);
+        }
+
+        let mut exports = ExportSection::new();
+        for (i, decl) in self.decls.iter().enumerate() {
+            if self.module.exports.contains(&decl.name) {
+                exports.export(&decl.name, ExportKind::Func, i as u32);
+            }
+        }
+
+        let mut codes = CodeSection::new();
+        for decl in self.decls.iter() {
+            let locals = vec![];
+            let mut f = Function::new(locals);
+            match decl.expr {
+                Expr::I32(i) => f.instruction(&Instruction::I32Const(i)),
+                _ => unreachable!(),
+            };
+            f.instruction(&Instruction::Return);
+            f.instruction(&Instruction::End);
+            codes.function(&f);
+        }
+
+        module.section(&types);
+        module.section(&functions);
+        module.section(&exports);
+        module.section(&codes);
+        module.finish()
     }
 }
